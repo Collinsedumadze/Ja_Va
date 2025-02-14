0815c3f8dbd1cfec8808c3ee230551555ba39f7c
Collinsedumadze <egyiredumadzecollins@gmail.com>
2025-02-14 00:24:57 +0000

    Properties of an account

---
+++ Account.java
@@ -0,0 +1,13 @@
+public class Account {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+    private String accountType;
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("I finally got Java in Visual studio code");
+    }
+}
--- start.java
+++
@@ -1,5 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("I finally got Java in Visual studio code");
-    }
-}
